493c60c001d58e2a491464ca4930c80656a861b9
Nicole <nyarbrough14@gmail.com>
2021-06-04 12:19:31 -0700

    Fixed tests for updated reverse.py

--- test_reverse.py
+++ test_reverse.py
@@ -1,6 +1,16 @@
 import reverse
 
-def test1():
-    values = ("baMbmaB")
+def testdefault():
+    values = ("My name is V Tadimeti")
     val = reverse.reverse(values)
-    assert val == "BambMab"
+    assert val == "Tadimeti V is name My"
+
+def testdefaultwithperiod():
+    values = ("My name is V Tadimeti.")
+    val = reverse.reverse(values)
+    assert val == "Tadimeti V is name My."
+
+def testwithnum():
+    values = ("baMbmaB testing with numbers 1 2 3 34")
+    val = reverse.reverse(values)
+    assert val == "34 3 2 1 numbers with testing baMbmaB"
