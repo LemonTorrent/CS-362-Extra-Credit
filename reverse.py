cb06d94c28e87fc3bfa306782714a72126e4c16b
Nicole <nyarbrough14@gmail.com>
2021-06-04 12:19:05 -0700

    Fixed misunderstanding on the functionality of reverse.py

--- reverse.py
+++ reverse.py
@@ -1,8 +1,20 @@
 def reverse( inputStr ):
     revStr = ""
-    for i in range(len(inputStr)):
-        revStr += inputStr[-(i+1)]
+    splitStr = inputStr.split()
+
+    if (splitStr[-1][-1] == "."):
+        splitStr[-1] = splitStr[-1][:-1]
+        for i in range(len(splitStr)-1):
+            revStr += splitStr[-(i+1)]
+            revStr += " "
+        revStr += splitStr[0]
+        revStr += "."
+    else:
+        for i in range(len(splitStr)-1):
+            revStr += splitStr[-(i+1)]
+            revStr += " "
+        revStr += splitStr[0]
 
     return revStr
 
-print(reverse("Nope not for Me"))
+#print(reverse("Nope not for Me."))
